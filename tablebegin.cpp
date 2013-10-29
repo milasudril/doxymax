722babb3c7225c0cdfff9dca5e7033e2b0cf7c29
unknown <milasudril@gmail.com>
2013-10-29 09:11:55 +0100

    table_begin did not update the Table counter

--- tablebegin.cpp
+++ tablebegin.cpp
@@ -14,27 +14,25 @@ void Doxymax::TableBegin::expand(const Macro& macro,CommentProcessor& processor)
 	{
 	if(macro.args.length()<2)
 		{throw Herbs::ExceptionMissing(___FILE__,__LINE__);}
-	else
+
+	Herbs::String str_out(64);
+	
+	str_out.append(STR("\\anchor ")).append(macro.args[1])
+		.append(STR(" <div style=\"width:100%\">"
+			"<div class=\"caption\" style=\"text-align:center\">Table "));
+	
+	size_t n=processor.labelGet(macro.args[1]);
+	if(n==0)
 		{
-		Herbs::String str_out(64);
-		
-		str_out.append(STR("\\anchor ")).append(macro.args[1])
-			.append(STR(" <div style=\"width:100%\">"
-				"<div class=\"caption\" style=\"text-align:center\">Table "));
-		
-		size_t n=processor.labelGet(macro.args[1]);
-		if(n==0)
-			{
-			n=processor.counterGet(STR("Table"));
-			++n;
-			processor.labelSet(macro.args[1],n);
-			}
-		
-		str_out.append(Herbs::IntFormat<size_t>(n)).append(STR(": "))
-			.append(macro.args[0])
-			.append(STR("</div>"))
-			.append(STR("<table class=\"doxtable\" style=\"margin-left:auto;margin-right:auto;\">"));
-		
-		print(str_out);
+		n=processor.counterGet(STR("Table"));
+		++n;
+		processor.labelSet(macro.args[1],n);
 		}
+	processor.counterSet(STR("Table"),n);
+	str_out.append(Herbs::IntFormat<size_t>(n)).append(STR(": "))
+		.append(macro.args[0])
+		.append(STR("</div>"))
+		.append(STR("<table class=\"doxtable\" style=\"margin-left:auto;margin-right:auto;\">"));
+	
+	print(str_out);
 	}
