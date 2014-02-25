1103d942ed646f9813dae3b6ded989c397ec9656
Torbjörn <milasudril@gmail.com>
2014-02-25 17:21:02 +0100

    Paragraph: More doxygen-like anchor

--- paragraph.cpp
+++ paragraph.cpp
@@ -13,7 +13,7 @@ void Doxymax::Paragraph::expand(const Macro& macro,CommentProcessor& processor)
 	if(macro.args.length()<2)
 		{throw Herbs::ExceptionMissing(___FILE__,__LINE__);}
 
-	print(Herbs::format(STR("\\htmlonly <div class=\"parabox\"><h4><a name=\"%1\">%0</a></h4><p class=\"parainline\">"),
+	print(Herbs::format(STR("\\htmlonly <div class=\"parabox\"><h4><a class=\"anchor\" id=\"%1\"></a>%0</h4><p class=\"parainline\">"),
 		{
 		 macro.args[0].begin()
 		,macro.args[1].begin()
